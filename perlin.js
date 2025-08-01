7aa1a8caef4c5b53f605b5e8ec48114d882c115c
lyam08888 <lyes.amara8@gmail.com>
2025-08-01 17:41:17 +0200

    Create perlin.js

---
+++ perlin.js
@@ -0,0 +1,27 @@
+// perlin.js
+// Ce module fournit un générateur de bruit de Perlin déterministe,
+// en utilisant le générateur de nombres pseudo-aléatoires.
+
+import { SeededRandom } from './seededRandom.js';
+
+export const Perlin = {
+    rand_vect: function(){ let theta = SeededRandom.random()*2*Math.PI; return {x:Math.cos(theta), y:Math.sin(theta)}; },
+    dot_prod_grid: function(x, y, vx, vy){
+        let g_vect; let d_vect = {x:x-vx, y:y-vy};
+        if (this.gradients[[vx,vy]]){ g_vect = this.gradients[[vx,vy]]; }
+        else { g_vect = this.rand_vect(); this.gradients[[vx,vy]] = g_vect; }
+        return d_vect.x*g_vect.x + d_vect.y*g_vect.y;
+    },
+    smootherstep: (x) => 6*x**5 - 15*x**4 + 10*x**3,
+    interp: function(x, a, b){ return a + this.smootherstep(x)*(b-a); },
+    seed: function(){ this.gradients = {}; this.memory = {}; },
+    get: function(x, y) {
+        if (this.memory.hasOwnProperty([x,y])) return this.memory[[x,y]];
+        let xf = Math.floor(x); let yf = Math.floor(y);
+        let tl = this.dot_prod_grid(x, y, xf,   yf); let tr = this.dot_prod_grid(x, y, xf+1, yf);
+        let bl = this.dot_prod_grid(x, y, xf,   yf+1); let br = this.dot_prod_grid(x, y, xf+1, yf+1);
+        let xt = this.interp(x-xf, tl, tr); let xb = this.interp(x-xf, bl, br);
+        let v = this.interp(y-yf, xt, xb); this.memory[[x,y]] = v;
+        return v;
+    }
+};
