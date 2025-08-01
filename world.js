5f6eb9c8130b9fa77432ea9035116f73f05099d7
lyam08888 <lyes.amara8@gmail.com>
2025-08-01 17:41:50 +0200

    Update world.js

--- world.js
+++ world.js
@@ -1,28 +1,6 @@
 import { randomChestType } from './chestGenerator.js';
 import { SeededRandom } from './seededRandom.js';
-
-// Perlin Noise Generator (déterministe)
-const Perlin = {
-    rand_vect: function(){ let theta = SeededRandom.random()*2*Math.PI; return {x:Math.cos(theta), y:Math.sin(theta)}; },
-    dot_prod_grid: function(x, y, vx, vy){
-        let g_vect; let d_vect = {x:x-vx, y:y-vy};
-        if (this.gradients[[vx,vy]]){ g_vect = this.gradients[[vx,vy]]; }
-        else { g_vect = this.rand_vect(); this.gradients[[vx,vy]] = g_vect; }
-        return d_vect.x*g_vect.x + d_vect.y*g_vect.y;
-    },
-    smootherstep: (x) => 6*x**5 - 15*x**4 + 10*x**3,
-    interp: function(x, a, b){ return a + this.smootherstep(x)*(b-a); },
-    seed: function(){ this.gradients = {}; this.memory = {}; },
-    get: function(x, y) {
-        if (this.memory.hasOwnProperty([x,y])) return this.memory[[x,y]];
-        let xf = Math.floor(x); let yf = Math.floor(y);
-        let tl = this.dot_prod_grid(x, y, xf,   yf); let tr = this.dot_prod_grid(x, y, xf+1, yf);
-        let bl = this.dot_prod_grid(x, y, xf,   yf+1); let br = this.dot_prod_grid(x, y, xf+1, yf+1);
-        let xt = this.interp(x-xf, tl, tr); let xb = this.interp(x-xf, bl, br);
-        let v = this.interp(y-yf, xt, xb); this.memory[[x,y]] = v;
-        return v;
-    }
-};
+import { Perlin } from './perlin.js';
 
 // --- LISTE DE BLOCS ---
 export const TILE = { 
@@ -141,7 +119,6 @@ export function generateLevel(game, levelConfig, gameSettings) {
             }
         }
         game.tileMap[worldHeightInTiles - 1][x] = TILE.BEDROCK;
-        // La ligne `game.tileMap[0][x] = TILE.BEDROCK;` a été supprimée.
     }
 
     // 7. Placer les arbres, fleurs, etc.
