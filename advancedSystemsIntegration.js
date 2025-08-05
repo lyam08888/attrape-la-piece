9bcf9f7bd0e2c6b4f8c2245d428a77f996dd32b8
Ly Am <lyes.amara8@gmail.com>
2025-08-05 10:40:35 +0200

    okokok

--- advancedSystemsIntegration.js
+++ advancedSystemsIntegration.js
@@ -4,7 +4,7 @@
 import { WeatherSystem as AdvancedWeatherSystem } from './weatherSystem.js';
 import { DisasterManager as DisasterManagerAdvanced } from './disasterManager.js';
 import { AnimalManager as AnimalSystemAdvanced } from './animalSystem.js';
-import { ExplorationSystem } from './explorationSystem.js';
+import ExplorationSystem from './explorationSystem.js';
 import { TimeSystem } from './timeSystem.js';
 import { LightingSystem as DynamicLightingSystem } from './lighting.js';
 import { WorldAnimator } from './worldAnimator.js';
